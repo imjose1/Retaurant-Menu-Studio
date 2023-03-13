b702ca08382caf45fbcf2bbec547e7ad9d0c2736
Jose g hernandez <jose_ghj@hotmail.com>
2023-03-13 16:13:14 -0500

    Add project files.

---
+++ Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retaurant_Menu_Studio
+{
+    public class Menu
+    {
+        public List<MenuItems> Items; { get; set; }
+        DateTime Adate; { get; set; }
+
+        public Menu(List<MenuItems> items, DateTime adate)
+        {
+            Items = items;
+            Adate = adate;
+        }
+
+        
+
+     
+    }
+}
---
+++ MenuItems.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retaurant_Menu_Studio
+{
+    public class MenuItems
+    {
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNew { get; set; }
+        public bool IsSpicy { get; set; }
+    }
+    public MenuItems(double price, string description, string category, bool isNew, bool isSpicy)
+        {
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNew = isNew;
+            IsSpicy = isSpicy;
+        }
+    }
+}
